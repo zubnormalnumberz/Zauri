dd97a77a44581fd5e27a344bca0d4b1ef1bb5936
Zubnormal Numberz <33485885+zubnormalnumberz@users.noreply.github.com>
2021-04-05 09:47:30 +0200

    Initial Commit

---
+++ Zauri/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Zauri
+//
+//  Created by Oihan Arroyo on 05/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
